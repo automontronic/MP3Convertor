b86ef98a8be562aefad05832908d032b36fdccf1
automontronic <40577941+automontronic@users.noreply.github.com>
2021-06-08 14:40:45 -0400

    Initial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace MP3Convertor
 {
@@ -6,7 +7,29 @@ namespace MP3Convertor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the source file:");
+            string source = Console.ReadLine();
+            Console.WriteLine("Enter the destination file:");
+            string destination = Console.ReadLine();
+            Execute(Environment.CurrentDirectory + "\\ffmpeg.exe", string.Format("-i \"{0}\" -hide_banner -ac 1 \"{1}\"",source, destination));
+        }
+
+        private static string Execute(string exePath, string parameters)
+        {
+            string result = String.Empty;
+            using (Process p = new Process())
+            {
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = exePath;
+                p.StartInfo.Arguments = parameters;
+                p.Start();
+                p.WaitForExit();
+
+                result = p.StandardOutput.ReadToEnd();
+            }
+            return result;
         }
     }
 }
