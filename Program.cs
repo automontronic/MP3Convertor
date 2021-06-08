0a915a3a75e7bf4be9ed10833d7ebc0496ab15ec
automontronic <40577941+automontronic@users.noreply.github.com>
2021-06-08 14:29:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MP3Convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
